84b22b5bb01832862aa6c986943a0c2dfb190203
FajarPhajenk <Fajarrcf09@gmail.com>
2022-07-19 15:29:55 +0700

    number 2

---
+++ script.js
@@ -0,0 +1,22 @@
+alert("Hai, nama aku bebeb");
+var tes = confirm("Apakah aku mengenal mu?");
+if (tes === true) {
+    alert("Hmm, sedikit ragu!");
+} else {
+    alert("Baik, dadah!!!");
+}
+var lagi = true;
+while (lagi === true) {
+    var nama = prompt("Perkenalkan nama kamu");
+    alert("halo! " + nama);
+    confirm("Itukah kamu " + nama);
+    alert("Wah ternyata itu benar kamu " + nama);
+    alert("Senang bertemu dengan kamu " + nama);
+    var tos = confirm("Bagaimana kabar kamu hari ini " + nama);
+    if (tos === true) {
+        alert("Bagus lah kalau begitu aku senang");
+    } else {
+        prompt("Apa kamu sudah kedokter " + nama);
+        alert("Semoga Lekas sembuh ya...");
+    }
+}
