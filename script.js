788f5bcc04febf50bfed698ad7b847b0fd99e4fd
FajarPhajenk <Fajarrcf09@gmail.com>
2022-07-19 15:45:41 +0700

    suasana

--- script.js
+++ script.js
@@ -6,17 +6,17 @@ if (tes === true) {
     alert("Baik, dadah!!!");
 }
 var lagi = true;
-while (lagi === true) {
-    var nama = prompt("Perkenalkan nama kamu");
-    alert("halo! " + nama);
-    confirm("Itukah kamu " + nama);
-    alert("Wah ternyata itu benar kamu " + nama);
-    alert("Senang bertemu dengan kamu " + nama);
-    var tos = confirm("Bagaimana kabar kamu hari ini " + nama);
-    if (tos === true) {
-        alert("Bagus lah kalau begitu aku senang");
-    } else {
-        prompt("Apa kamu sudah kedokter " + nama);
-        alert("Semoga Lekas sembuh ya...");
-    }
+//while (lagi === true) {
+var nama = prompt("Perkenalkan nama kamu");
+alert("halo! " + nama);
+confirm("Itukah kamu " + nama);
+alert("Wah ternyata itu benar kamu " + nama);
+alert("Senang bertemu dengan kamu " + nama);
+var tos = confirm("Bagaimana kabar kamu hari ini " + nama);
+if (tos === true) {
+    alert("Bagus lah kalau begitu aku senang");
+} else {
+    prompt("Apa kamu sudah kedokter " + nama);
+    alert("Semoga Lekas sembuh ya...");
+}
 }
